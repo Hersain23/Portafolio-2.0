be06c85051cb115970106cedfaf84d6fc5c9bb68
FacebookConafe <conafe.facebook@gmail.com>
2023-02-10 23:32:38 -0600

    Prototipo del portafolio final

--- JS/Transiciones.js
+++ JS/Transiciones.js
@@ -1,25 +1,8 @@
 let cajaHabilidad = document.querySelectorAll(".habilidades");
-let imagenes = ["/Recursos/Youtube.png","/Recursos/Bars.png","/Recursos/PokeApi.png"];
-let flechas = document.querySelectorAll(".controlador");
-let contadorImagen = 0;
-let imagenCarrusel = document.getElementById("img");
-
-document.addEventListener("DOMContentLoaded",()=>{
-    let i = 0;
-      
-})
-
-function setImagen(i){   
-    return new Promise((resolve,reject)=>{
-        setInterval(() => {
-            
-        }, 3000);
-    })
-}
 cajaHabilidad.forEach(element => {
     element.addEventListener("mousemove",(e)=>{
             element.style.transform = "scale(1.1)"
-            element.style.filter = "brightness(1.5)"
+           /* element.style.filter = "brightness(1.5)"*/
             
     })
 });
@@ -27,14 +10,6 @@ cajaHabilidad.forEach(element => {
 cajaHabilidad.forEach(element => {
     element,addEventListener("mouseout",(e)=>{
         element.style.transform = "scale(1)"
-        element.style.filter = "brightness(1)"
+       /* element.style.filter = "brightness(1)"*/
     })
 });
-
-flechas[0].addEventListener("click",()=>{
-    
-})
-
-flechas[1].addEventListener("click",()=>{
-
-})
---
+++ JS/carrusel.js
@@ -0,0 +1,76 @@
+let imagenes = ["../Recursos/Youtube.png","../Recursos/Bars.png","../Recursos/PokeApi.PNG"];
+let flechas = document.querySelectorAll(".controlador");
+
+let izquierda = document.getElementById("izquierda");
+let derecha = document.getElementById("derecha");
+let pos = 0;
+
+izquierda.addEventListener("click",()=>{
+    if(pos == 0){
+        let promise = new Promise((resolve, reject)=>{
+        resolve( 
+            document.querySelector(".imagen").style.opacity = "0"
+        )
+        })
+        promise
+        .then(()=>{
+            setTimeout(()=>{
+            document.querySelector(".imagen").style.opacity = "1"
+            document.querySelector(".imagen").src = imagenes[2];
+            pos = 2;
+        },1100)
+        })
+        .catch()
+    }else{
+        pos--;
+        let promise = new Promise((resolve, reject)=>{
+            resolve( 
+                document.querySelector(".imagen").style.opacity = "0"
+            )
+            })
+            promise
+            .then(()=>{
+                setTimeout(()=>{
+                document.querySelector(".imagen").style.opacity = "1"
+                document.querySelector(".imagen").src = imagenes[pos];
+            },1100)
+            })
+            .catch()
+    }
+});
+
+derecha.addEventListener("click",()=>{
+    if(pos == 2){
+        pos = 0;
+        let promise = new Promise((resolve, reject)=>{
+            resolve( 
+                document.querySelector(".imagen").style.opacity = "0"
+            )
+            })
+            promise
+            .then(()=>{
+                setTimeout(()=>{
+                document.querySelector(".imagen").style.opacity = "1"
+                document.querySelector(".imagen").src = imagenes[pos];
+            },1100)
+            })
+            .catch()
+    }
+    else{
+        pos++;
+        let promise = new Promise((resolve, reject)=>{
+            resolve( 
+                document.querySelector(".imagen").style.opacity = "0"
+            )
+            })
+            promise
+            .then(()=>{
+                setTimeout(()=>{
+                document.querySelector(".imagen").style.opacity = "1"
+                document.querySelector(".imagen").src = imagenes[pos];
+            },1100)
+            })
+            .catch()
+        
+    }
+})
